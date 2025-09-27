74219585c9efa93d9d80a84ed3982a6f7ca2ce9e
Ibrahim Bazra <baim.weng12@gmail.com>
2025-09-27 19:59:50 +0700

    misah code pc dan mobile

--- resources/views/home/index/teaser.blade.php
+++ resources/views/home/index/teaser.blade.php
@@ -1,50 +1,88 @@
 <div class="bg-[#FFEAC8] mix-blend-multiply">
   <div class="pb-40" id="teaser">
-    <!-- Teaser Section -->
-    <div class="relative"> <!-- relative pindah ke sini -->
-      
-      <!-- Header -->
-      <div class="grid lg:grid-cols-1 grid-cols-1 md:gap-6 h-auto align-items-start lg:align-items-stretch lg:items-center lg:py-0">
-        <div class="lg:pl-16 lg:pr-16 md:px-8 sm:px-2 col-span-1 relative lg:pt-20 md:pt-20 pt-12 pb-10 lg:pb-20 lg:block"
-             data-aos="zoom-in-up">
-          <div class="flex flex-row justify-between items-center rounded-3xl lg:w-full m-auto shadow-xl static bg-[#8B2F4B] overflow-hidden bg-opacity-95 mt-[50px]"
-               style="height:7.625rem">
-            <div class="flex flex-row lg:pl-7 md:px-4 px-2">
-              <img src="{{ elemen1 }}" alt="ornamen kiri" class="h-12 w-auto">
-            </div>
-            <div class="lg:text-center text-center lg:ml-0 lg:mr-0">
-              <h1 class="z-20 font-brasikaDisplay lg:text-4xl md:text-3xl text-2xl leading-none md:mb-1 text-[#F9C46B]">
+    <div class="relative">
+
+      <!-- ================== HP ================== -->
+      <div class="block lg:hidden">
+        <!-- Header -->
+        <div class="grid grid-cols-1 md:gap-6 h-auto align-items-start py-12 pb-10" data-aos="zoom-in-up">
+          <div class="col-span-1 relative">
+            <div class="flex flex-col items-center justify-center rounded-2xl bg-[#8B2F4B] bg-opacity-95 shadow-xl py-4">
+              <img src="{{ elemen1 }}" alt="ornamen kiri" class="h-10 w-auto mb-2">
+              <h1 class="font-brasikaDisplay text-2xl text-[#F9C46B] text-center">
                 Teaser PKKMB-PKBN 2025
               </h1>
+              <img src="{{ elemen1 }}" alt="ornamen kanan" class="h-10 w-auto mt-2 scale-x-[-1]">
             </div>
-            <div class="flex flex-row lg:pr-7 md:px-4 px-2">
-              <img src="{{ elemen1 }}" alt="ornamen kanan" class="h-12 w-auto">
+          </div>
+
+          <!-- Frame maroon -->
+          <div class="relative bg-[#8B1E3F] p-2 mt-6 rounded-lg shadow-lg w-[95vw] mx-auto">
+            <div class="aspect-video bg-black rounded-md overflow-hidden">
+              <iframe class="w-full h-full"
+                      src="https://www.youtube.com/embed/56hq1-sRfHg"
+                      title="Teaser Video"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen>
+              </iframe>
             </div>
           </div>
         </div>
 
-        <!-- Frame maroon -->
-        <div class="relative bg-[#8B1E3F] p-3 mt-10 rounded-lg shadow-lg w-fit mx-auto mb-[-75px]">
-          <div class="w-[90vw] max-w-4xl aspect-video bg-black rounded-md overflow-hidden">
-            <iframe class="w-full h-full"
-                    src="https://www.youtube.com/embed/56hq1-sRfHg"
-                    title="Teaser Video"
-                    frameborder="0"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowfullscreen>
-            </iframe>
-          </div>
-        </div>
+        <!-- Ornamen -->
+        <img src="img/asset/2025/tugu 0 km.png"
+             alt="tugu 0 km"
+             class="absolute bottom-0 left-2 w-[100px] z-10 mb-[-120px]">
+        <img src="img/asset/2025/maleo.png"
+             alt="burung maleo"
+             class="absolute bottom-0 right-2 w-[120px] z-10 mb-[-150px]">
       </div>
 
-      <!-- Ornamen tambahan -->
-      <img src="img/asset/2025/tugu 0 km.png" 
-           alt="tugu 0 km" 
-           class="absolute bottom-0 left-4 w-[171px] h-[419px] h-auto z-10 mb-[-240px] ml-[50px]">
+      <!-- ================== PC/Laptop ================== -->
+      <div class="hidden lg:block">
+        <!-- Header -->
+        <div class="grid lg:grid-cols-1 grid-cols-1 md:gap-6 h-auto align-items-start lg:py-0">
+          <div class="lg:pl-16 lg:pr-16 col-span-1 relative lg:pt-20 pb-10 lg:pb-20 lg:block"
+               data-aos="zoom-in-up">
+            <div class="flex flex-row justify-between items-center rounded-3xl lg:w-full m-auto shadow-xl static bg-[#8B2F4B] overflow-hidden bg-opacity-95 mt-[50px]"
+                 style="height:7.625rem">
+              <div class="flex flex-row lg:pl-7 md:px-4 px-2">
+                <img src="{{ elemen1 }}" alt="ornamen kiri" class="h-12 w-auto">
+              </div>
+              <div class="text-center">
+                <h1 class="font-brasikaDisplay text-4xl md:text-3xl text-2xl text-[#F9C46B]">
+                  Teaser PKKMB-PKBN 2025
+                </h1>
+              </div>
+              <div class="flex flex-row lg:pr-7 md:px-4 px-2">
+                <img src="{{ elemen1 }}" alt="ornamen kanan" class="h-12 w-auto">
+              </div>
+            </div>
+          </div>
 
-      <img src="img/asset/2025/maleo.png" 
-           alt="burung maleo" 
-           class="absolute bottom-0 right-4 w-[200px] h-[239px] z-10 mb-[-280px] mr-[50px]">
+          <!-- Frame maroon -->
+          <div class="relative bg-[#8B1E3F] p-3 mt-10 rounded-lg shadow-lg w-fit mx-auto mb-[-75px]">
+            <div class="w-[90vw] max-w-4xl aspect-video bg-black rounded-md overflow-hidden">
+              <iframe class="w-full h-full"
+                      src="https://www.youtube.com/embed/56hq1-sRfHg"
+                      title="Teaser Video"
+                      frameborder="0"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                      allowfullscreen>
+              </iframe>
+            </div>
+          </div>
+        </div>
+
+        <!-- Ornamen -->
+        <img src="img/asset/2025/tugu 0 km.png"
+             alt="tugu 0 km"
+             class="absolute bottom-0 left-4 w-[171px] h-[419px] z-10 mb-[-240px] ml-[50px]">
+        <img src="img/asset/2025/maleo.png"
+             alt="burung maleo"
+             class="absolute bottom-0 right-4 w-[200px] h-[239px] z-10 mb-[-280px] mr-[50px]">
+      </div>
     </div>
   </div>
 </div>
--- resources/views/home/index/visi-misi.blade.php
+++ resources/views/home/index/visi-misi.blade.php
@@ -1,70 +1,108 @@
 <div class="bg-[#FFEAC8] mix-blend-multiply">
-  <!-- Header -->
-  <div
-    class="grid lg:grid-cols-1 grid-cols-1 md:gap-6 h-auto align-items-start lg:align-items-stretch lg:items-center lg:py-0">
-    <div class="lg:pl-16 lg:pr-16 md:px-8 sm:px-2 col-span-1 relative lg:pt-20 md:pt-20 pt-12 lg:block"
-      data-aos="zoom-in-up">
-      <div
-        class="flex flex-row justify-between items-center rounded-3xl lg:w-full m-auto shadow-xl static bg-[#8B2F4B] overflow-hidden "
-        style="height:7.625rem">
-        <div class="flex flex-row lg:pl-7 md:px-4 px-2 justify-center items-center">
-          <img src={{ elemen1 }}
-            class="h-[7rem] lg:h-[12rem] w-auto lg:absolute lg:left-0 m-auto lg:p-[inherit]">
+
+  <!-- ================= TAMPIILAN HP ================= -->
+  <div class="block lg:hidden">
+    <!-- Header HP -->
+    <div class="col-span-1 relative pt-12 px-4" data-aos="zoom-in-up">
+      <div class="flex flex-col justify-center items-center rounded-2xl w-full m-auto shadow-xl bg-[#8B2F4B] overflow-hidden" style="height:auto">
+        <img src="{{ elemen1 }}" class="h-[7rem] w-auto mb-2">
+        <h1 class="font-brasikaDisplay text-2xl text-[#F9C46B] text-center">
+          Visi dan Misi PKKMB-PKBN 2024
+        </h1>
+        <img src="{{ elemen1 }}" class="h-[7rem] w-auto mt-2 scale-x-[-1]">
+      </div>
+    </div>
+
+    <!-- Visi HP -->
+    <div class="py-8 px-4">
+      <div class="bg-[#8B2F4B] text-white px-6 py-8 rounded-xl relative text-center shadow-lg">
+        <div class="z-0 absolute inset-0 bg-cover opacity-[20%]"
+             style="background-image: url('img/asset/2025/timeline/patternwait1.png'); background-position: center;">
         </div>
-        <div class="lg:text-center text-center lg:ml-0 lg:mr-0">
-          <h1 class="z-20 font-brasikaDisplay lg:text-4xl md:text-3xl text-2xl leading-none md:mb-1 text-[#F9C46B]"
-            id="title-visimisi">
+        <h2 class="text-lg font-semibold mb-3 font-poppins tracking-widest">VISI:</h2>
+        <p class="text-base leading-relaxed font-nunito relative z-10">
+          “Membangun Mahasiswa yang Tangguh Serta Siap Menempuh Pendidikan
+          Untuk Menjadi Calon ASN Ahli di Bidang Statistik dan Komputasi Statistik yang BerAKHLAK”
+        </p>
+      </div>
+    </div>
+
+    <!-- Misi HP -->
+    <div class="py-6 px-4">
+      <div class=" text-white px-6 py-8 rounded-2xl relative shadow-lg overflow-hidden">
+        <div class="z-0 absolute inset-0 bg-cover opacity-[20%]"
+             style="background-image: url('img/asset/2025/timeline/patternwait1.png'); background-position: center;">
+        </div>
+        <h2 class="text-center text-lg font-semibold mb-4 font-poppins tracking-widest relative z-10">MISI:</h2>
+        <ol class="text-base leading-relaxed font-poppins list-decimal pl-4 relative z-10">
+          <li>Menumbuhkan ketaqwaan kepada Tuhan Yang Maha Esa.</li>
+          <li>Merancang kegiatan untuk memaksimalkan komunikasi...</li>
+          <li>Merancang kegiatan yang mengedepankan keaktifan...</li>
+          <li>Membentuk mahasiswa yang mempunyai budi pekerti...</li>
+          <li>bla bla bla...</li>
+        </ol>
+      </div>
+    </div>
+  </div>
+
+  <!-- ================= TAMPIILAN LAPTOP/PC ================= -->
+  <div class="hidden lg:block">
+    <!-- Header PC -->
+    <div class="lg:pl-16 lg:pr-16 col-span-1 relative lg:pt-20" data-aos="zoom-in-up">
+      <div class="flex flex-row justify-between items-center rounded-3xl w-full m-auto shadow-xl bg-[#8B2F4B] overflow-hidden" style="height:7.625rem">
+        <div class="flex flex-row lg:pl-7 justify-center items-center">
+          <img src="{{ elemen1 }}" class="h-[12rem] w-auto lg:absolute lg:left-0">
+        </div>
+        <div class="text-center">
+          <h1 class="font-brasikaDisplay text-4xl leading-none text-[#F9C46B]">
             Visi dan Misi PKKMB-PKBN 2024
           </h1>
         </div>
-        <div class="flex flex-row lg:pr-7 md:px-4 px-2 justify-center items-center">
-          <img src= {{ elemen1 }}
-            class="h-[7rem] lg:h-[12rem] w-auto lg:absolute lg:right-0 m-auto lg:p-[inherit] scale-x-[-1]">
+        <div class="flex flex-row lg:pr-7 justify-center items-center">
+          <img src="{{ elemen1 }}" class="h-[12rem] w-auto lg:absolute lg:right-0 scale-x-[-1]">
         </div>
       </div>
     </div>
 
+    <!-- Visi PC -->
     <div class="py-10 lg:pb-20">
-      <div class="flex sm:flex-row flex-col mx-auto w-full justify-center items-stretch mb-8 sm:px-12 px-4" data-aos="fade right">
+      <div class="flex flex-row mx-auto w-full justify-center items-stretch mb-8 px-12" data-aos="fade-right">
         <div class="flex-1 p-4">
-          <div class="bg-[#8B2F4B] content-center text-white px-8 py-12 rounded-lg h-full text-center"
-            style="box-shadow: 10px 12px 3px rgba(0, 0, 0, 0.5)">
+          <div class="bg-[#8B2F4B] text-white px-8 py-12 rounded-lg h-full text-center relative shadow-xl">
             <div class="z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-cover opacity-[20%]"
-               style="background-image: url('img/asset/2025/timeline/patternwait1.png'); background-position: center;  width: 50%; height: 87%;"></div>
-            <h2 class="sm:text-2xl text-lg font-semibold mb-4 font-poppins tracking-widest">VISI:</h2>
-            <p class="sm:text-2xl text-lg leading-relaxed font-nunito">
-              “Membangun Mahasiswa yang Tangguh Serta Siap Menempuh Pendidikan Untuk Menjadi Calon ASN Ahli di Bidang Statistik dan Komputasi Statistik yang BerAKHLAK”
+                 style="background-image: url('img/asset/2025/timeline/patternwait1.png'); background-position: center; width:50%; height:87%;">
+            </div>
+            <h2 class="text-2xl font-semibold mb-4 font-poppins tracking-widest">VISI:</h2>
+            <p class="text-2xl leading-relaxed font-nunito relative z-10">
+              “Membangun Mahasiswa yang Tangguh Serta Siap Menempuh Pendidikan
+              Untuk Menjadi Calon ASN Ahli di Bidang Statistik dan Komputasi Statistik yang BerAKHLAK”
             </p>
           </div>
         </div>
       </div>
 
-
-    <div class="flex sm:flex-row flex-col-reverse mx-auto w-full justify-center items-stretch sm:px-12 px-8 py-6" data-aos="fade left">
-      <div class="flex-1 p-4">
-        <div class="bg-[#8B2F4B] text-white px-8 py-12 rounded-[50px] h-full relative overflow-hidden"
-          style="box-shadow: -10px 12px 3px rgba(0, 0, 0, 0.5)">
-          <div class="z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-cover opacity-[20%]"
-               style="background-image: url('img/asset/2025/timeline/patternwait1.png'); background-position: center;  width: 50%; height: 100%;"></div>
-          <h2 class="text-center sm:text-2xl text-lg font-semibold mb-4 font-poppins tracking-widest">MISI:</h2>
-          <ol class="sm:text-2xl text-lg leading-relaxed font-poppins list-decimal pl-6">
-            <li>Menumbuhkan ketaqwaan kepada Tuhan Yang Maha Esa.</li>
-            <li>Merancang kegiatan untuk memaksimalkan komunikasi dan eksplorasi mahasiswa baru sehingga dapat
-              beradaptasi dalam ekosistem kampus.</li>
-            <li>Merancang kegiatan yang mengedepankan keaktifan dan kontribusi mahasiswa baru dalam memahami visi misi
-              Politeknik Statistika STIS maupun BPS, serta menerapkan core value ASN, BerAKHLAK.</li>
-            <li>Membentuk mahasiswa yang mempunyai budi pekerti baik, inisiatif, jiwa inspiratif, dan sikap
-              nasionalisme.</li>
-            <li>bla bla bla bla bla bla bla bla blab albalbalblfawbflawbflwafblwablfwabflwbafwabflwa</li>
-            <li>bla bla bla bla bla bla bla bla blab albalbalblfawbflawbflwafblwablfwabflwbafwabflwa</li>
-            <li>bla bla bla bla bla bla bla bla blab albalbalblfawbflawbflwafblwablfwabflwbafwabflwa</li>
-            <li>bla bla bla bla bla bla bla bla blab albalbalblfawbflawbflwafblwablfwabflwbafwabflwa</li>
-            <li>bla bla bla bla bla bla bla bla blab albalbalblfawbflawbflwafblwablfwabflwbafwabflwa</li>
-          </ol>
+      <!-- Misi PC -->
+      <div class="flex flex-row-reverse mx-auto w-full justify-center items-stretch px-12 py-6" data-aos="fade-left">
+        <div class="flex-1 p-4">
+          <div class="bg-[#8B2F4B] text-white px-8 py-12 rounded-[50px] h-full relative overflow-hidden shadow-xl">
+            <div class="z-0 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-cover opacity-[20%]"
+                 style="background-image: url('img/asset/2025/timeline/patternwait1.png'); background-position: center; width:50%; height:100%;">
+            </div>
+            <h2 class="text-center text-2xl font-semibold mb-4 font-poppins tracking-widest relative z-10">MISI:</h2>
+            <ol class="text-2xl leading-relaxed font-poppins list-decimal pl-6 relative z-10">
+              <li>Menumbuhkan ketaqwaan kepada Tuhan Yang Maha Esa.</li>
+              <li>Merancang kegiatan untuk memaksimalkan komunikasi...</li>
+              <li>Merancang kegiatan yang mengedepankan keaktifan...</li>
+              <li>Membentuk mahasiswa yang mempunyai budi pekerti...</li>
+              <li>bla bla bla...</li>
+              <li>bla bla bla...</li>
+              <li>bla bla bla...</li>
+              <li>bla bla bla...</li>
+              <li>bla bla bla...</li>
+            </ol>
+          </div>
         </div>
       </div>
-    </div>
-
     </div>
   </div>
 </div>
