7091701d3461f395512338cf8ce8e56188d784f3
Ibrahim Bazra <baim.weng12@gmail.com>
2025-09-28 20:03:08 +0700

    page maskot mobile

--- resources/views/home/index/maskot.blade.php
+++ resources/views/home/index/maskot.blade.php
@@ -64,6 +64,53 @@
 
    <!-- ================= TAMPIILAN HP ================= -->
   <div class="block lg:hidden">
+    <div class="relative flex justify-center">
+  <!-- Selendang -->
+  <img src="img/asset/2025/motif_.jpg" alt="Selendang" 
+       class="w-full h-24 object-cover rounded-2xl">
 
+  <!-- Kontainer judul di atas selendang -->
+  <div class="absolute inset-0 flex items-center justify-center">
+    <div class="bg-[#8B2F4B] px-8 py-3 rounded-2xl w-[370px] text-center">
+      <h1 class="text-[14px] text-[#F9C46B] font-brasikaDisplay tracking-wide">
+        Maskot PKKMB - PKBN 2025
+      </h1>
+    </div>
+  </div>
+</div>
+
+  <!-- Konten Utama -->
+  <main class="relative w-full min-h-screen bg-no-repeat bg-center bg-cover" style="background-image: url('img/asset/2025/Pulau Sulawesi.png'); background-size: 100% ;">
+
+  <!-- Maskot kiri (Tora) -->
+    <div class="absolute top-[100px] left-[44px] flex items-center space-x-4 animate-bounce-slow">
+    <!-- Gambar Maskot -->
+        <img src="{{ maskot0 }}" alt="Tora" class="w-[90px] h-[156px]">
+
+    <!-- Bubble Chat -->
+        <div class="relative bg-white text-black p-4 rounded-full w-[255px] h-[113px] shadow-lg">
+            <p class="text-sm md:text-base leading-relaxed">
+            Halo aku <b>Tora</b>, Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor.
+            </p>
+        <!-- ekor ke kiri -->
+            <div class="absolute top-1/2 -left-3 transform -translate-y-1/2 w-0 h-0 border-t-[12px] border-b-[12px] border-r-[16px] border-t-transparent border-b-transparent border-r-white"></div>
+        </div>
+    </div>
+
+    <!-- Maskot kanan (Tira) -->
+    <div class="absolute bottom-[200px] right-15 flex items-center space-x-4 animate-bounce-slow">
+     <!-- Bubble Chat -->
+        <div class="relative bg-white text-black p-4 rounded-full w-[255px] h-[113px] shadow-lg">
+            <p class="text-sm md:text-base leading-relaxed">
+            Halo aku <b>Tira</b>, Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor.
+            </p>
+        <!-- ekor ke kanan -->
+            <div class="absolute top-1/2 -right-3 transform -translate-y-1/2 w-0 h-0 border-t-[12px] border-b-[12px] border-l-[16px] border-t-transparent border-b-transparent border-l-white"></div>
+        </div>
+
+    <!-- Gambar Maskot -->
+    <img src="{{ maskot4 }}" alt="Tira" class="w-[93px] h-[150px]">
+    </div>
+    </main>
   </div>
 </div>
