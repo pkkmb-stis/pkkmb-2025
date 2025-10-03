d8b59d1ad1cda8cf5768b39ef68b2cbda03efa7e
engabuu35 <arbudibudibudi@gmail.com>
2025-10-03 18:29:30 +0700

    timelineseeder edit

--- database/seeders/TimelineSeeder.php
+++ database/seeders/TimelineSeeder.php
@@ -18,58 +18,64 @@ class TimelineSeeder extends Seeder
         $data = [
             [
                 'title' => 'Pendaftaran Ulang Online',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-05 01:00:00'),
-                'waktu_akhir' => Carbon::createFromTimeString('2024-09-06 01:00:00'),
+                'waktu_mulai' => Carbon::createFromTimeString('2025-10-06 01:00:00'),
+                'waktu_akhir' => Carbon::createFromTimeString('2025-10-06 01:00:00'),
                 'location' => 'Website Politeknik Statistika STIS'
             ],
             [
                 'title' => 'Verifikasi Berkas',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-10 01:00:00'),
-                'waktu_akhir' => Carbon::createFromTimeString('2024-09-11 01:00:00'),
+                'waktu_mulai' => Carbon::createFromTimeString('2025-10-09 01:00:00'),
+                'waktu_akhir' => Carbon::createFromTimeString('2025-10-10 23:59:00'),
                 'location' => 'Politeknik Statistika STIS'
             ],
             [
                 'title' => 'Penyambutan Mahasiswa Baru & Pra PKKMB',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-12 01:00:00'),
-                'caption' => 'Langkah Awal Mewujudkan Mahasiswa Adaptif yang Siap Berkarya di Lingkungan Baru',
+                'waktu_mulai' => Carbon::createFromTimeString('2025-10-11 01:00:00'),
+                'waktu_akhir' => Carbon::createFromTimeString('2025-10-12 01:00:00'),
+                'caption' => 'Langkah Awal Bersama, Bangun Semangat Menjadi Mahasiswa Seutuhnya',
                 'location' => 'Politeknik Statistika STIS'
             ],
             [
                 'title' => 'PKKMB Hari Ke-1',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-13 01:00:00'),
-                'caption' => 'Kenal BPS Lebih Dekat, Ciptakan Data Akurat!',
-                'location' => 'Badan Pusat Statistik RI & Politeknik Statistika STIS'
+                'waktu_mulai' => Carbon::createFromTimeString('2025-11-07 01:00:00'),
+                'caption' => 'Mengukuhkan Identitas Sebagai Mahasiswa Dalam Rangka Menjadi Calon Statistisi Profesional yang Beretika',
+                'location' => 'Politeknik Statistika STIS'
             ],
             [
                 'title' => 'PKKMB Hari Ke-2',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-14 01:00:00'),
+                'waktu_mulai' => Carbon::createFromTimeString('2025-11-08 01:00:00'),
                 'caption' => 'Membangun Diri sebagai Mahasiswa Bermental Tangguh dalam Mengarungi Kehidupan Berorganisasi',
                 'location' => 'Politeknik Statistika STIS'
             ],
             [
                 'title' => 'PKKMB Hari Ke-3',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-15 01:00:00'),
-                'caption' => 'Membangun Mahasiswa yang Bebas Narkoba, Beretika, dan Berkualitas',
+                'waktu_mulai' => Carbon::createFromTimeString('2025-11-14 01:00:00'),
+                'caption' => '',
                 'location' => 'Politeknik Statistika STIS'
             ],
             [
                 'title' => 'PKKMB Hari Ke-4',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-16 01:00:00'),
-                'caption' => 'Membangun Semangat Nasionalisme dan Kesadaran Mahasiswa dalam Menangkal LGBT dan Disinformasi',
+                'waktu_mulai' => Carbon::createFromTimeString('2025-11-15 01:00:00'),
+                'caption' => 'Memanfaatkan Sosial Media dalam Membangun Personal Branding Generasi Muda Anti Narkoba dan Berintegritas',
                 'location' => 'Politeknik Statistika STIS'
             ],
             [
-                'title' => 'PKBN',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-17 01:00:00'),
-                'waktu_akhir' => Carbon::createFromTimeString('2024-09-20 01:00:00'),
-                'caption' => 'Pembinaan Kesadaran Bela Negara',
-                'location' => 'Pusat Pendidikan Jasmani Kodiklat TNI AD'
+                'title' => 'PKKMB Hari Ke-5',
+                'waktu_mulai' => Carbon::createFromTimeString('2025-11-21 01:00:00'),
+                'caption' => 'Literasi Digital dan Penguatan Karakter Mahasiswa dalam Pencegahan Perilaku LGBT',
+                'location' => 'Politeknik Statistika STIS'
             ],
             [
-                'title' => 'Inaugurasi Angkatan 66',
-                'waktu_mulai' => Carbon::createFromTimeString('2024-09-28 01:00:00'),
-                'caption' => 'Menjadi Mahasiswa Unggul Melalui Bakat, Karya, dan Kreatifitas',
+                'title' => 'PKKMB Hari Ke-6',
+                'waktu_mulai' => Carbon::createFromTimeString('2025-11-22 01:00:00'),
+                'caption' => 'Optimalisasi Potensi Mahasiswa Melalui Pengelolaan Keuangan yang Bijak dan Kesehatan Mental yang Seimbang',
                 'location' => 'Politeknik Statistika STIS'
+            ],
+            [
+                'title' => 'Inaugurasi Angkatan 67',
+                'waktu_mulai' => Carbon::createFromTimeString('2025-12-19 01:00:00'),
+                'caption' => 'Melangkah Bersama, Menjadi Generasi Kedinasan BerAKHLAK dan Berintegritas',
+                'location' => 'Auditorium Politeknik Statistika STIS'
             ],
         ];
 
