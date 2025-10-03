6b331c3375912a7131aad873ee7d2ccf7001c8e4
Ibrahim Bazra <baim.weng12@gmail.com>
2025-10-04 00:14:45 +0700

    animasi selendang

--- resources/views/home/index/transisi-selendang-hero-teaser.blade.php
+++ resources/views/home/index/transisi-selendang-hero-teaser.blade.php
@@ -2,32 +2,35 @@
 <div class="relative z-0 w-full h-[80px] lg:h-[133px] hidden md:block bg-[#1e2a4ad8] mt-[-40px] flex items-center justify-center">
   
   <!-- Selendang Atas -->
-  <img src="img/asset/2025/selendang 1.png" 
-       alt="Selendang 1"
-       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[200px] lg:h-[300px]  w-full animate-selendang">
-  
+ <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[200px] lg:h-[300px] w-screen overflow-hidden">
+     <div class="absolute w-full h-full animate-swayslow1">
+          <img src="img/asset/2025/selendang 1.png" class="w-full h-[200px] lg:h-[300px]" />
+     </div>
+ </div>   
   <!-- Selendang Bawah -->
-  <img src="img/asset/2025/selendang 2.png" 
-       alt="Selendang 2"
-       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[200px] lg:h-[300px] w-full animate-selendang-slow">
-
+ <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[200px] lg:h-[300px] w-screen overflow-hidden">
+     <div class="absolute w-full h-full animate-swayslow2 ">
+          <img src="img/asset/2025/selendang 2.png" class="w-full h-[200px] lg:h-[300px]" />
+     </div>
+ </div>
 </div>
 
 
-<!-- SELENDANG TRANSISI MOBILE -->
-<div class="relative z-0 w-full md:h-[80px] overflow-visible -mt-1 block md:hidden">
-  <!-- Layer Atas -->
-    <!-- Selendang Atas -->
-  <img src="img/asset/2025/selendang 1.png" 
-       alt="Selendang 1"
-       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-              h-[140px] sm:h-[180px] md:h-[150px] w-full 
-              animate-selendang">
+<!-- Selendang untuk MOBILE -->
+<div class="relative z-0 w-full h-[60px] sm:h-[80px] md:hidden bg-[#1e2a4ad8] flex items-center justify-center">
 
-  <!-- Selendang Bawah -->
-  <img src="img/asset/2025/selendang 2.png" 
-       alt="Selendang 2"
-       class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
-              h-[140px] sm:h-[180px] md:h-[150px] w-full 
-              animate-selendang-slow">
+  <!-- Selendang Atas (mobile) -->
+  <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[120px] sm:h-[160px] w-[150%] overflow-hidden">
+    <div class="absolute w-full h-full animate-swayslow1">
+      <img src="img/asset/2025/selendang 1.png" class="w-full h-[120px] sm:h-[160px]" />
+    </div>
+  </div>
+
+  <!-- Selendang Bawah (mobile) -->
+  <div class="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-[120px] sm:h-[160px] w-[150%] overflow-hidden">
+    <div class="absolute w-full h-full animate-swayslow2">
+      <img src="img/asset/2025/selendang 2.png" class="w-full h-[120px] sm:h-[160px]" />
+    </div>
+  </div>
+
 </div>
