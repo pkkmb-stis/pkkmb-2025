92643cf96e36ea73b0576d28f86eb3ffad073ed7
engabuu35 <arbudibudibudi@gmail.com>
2025-10-03 22:00:01 +0700

    edit perloginan 99%

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -9,14 +9,15 @@
     <form method="POST" action="{{ route('password.email') }}">
         @csrf
 
-        <div class="block">
-            <x-label for="username" value="No Ujian / Username" class="mb-2" />
-            <x-input id="username" class="block w-full mt-1" type="text" name="username" :value="old('username')"
-                required autofocus />
+        <div>
+            <x-label for="username" value="No Ujian / Username" class="mb-2 font-poppins" />
+            <input type="text" :value="old('username')"
+                class="block w-full px-4 py-2 mt-1 rounded-full bg-white shadow-[0_0_20px_rgba(0,0,0,0.05)] border border-transparent focus:outline-none focus:ring-2 focus:ring-brown-600 focus:border-transparent"
+                name="username" required autofocus>
         </div>
 
-        <div class="mt-3">
-            <x-button class="w-full mt-4 text-lg transition bg-gradient-to-b from-pink-500 to-indigo-900 hover:brightness-110 hover:scale-105 font-poppins">
+        <div class="flex flex-col items-center mt-4">
+            <x-button class="inline-flex items-center justify-center px-8 py-3 text-lg font-bold text-white transition-all rounded-full shadow-lg bg-gradient-to-b from-pink-500 to-indigo-900 hover:brightness-110 hover:scale-105 font-poppins">
                 Reset Password
             </x-button>
 
--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -7,24 +7,26 @@
 
         <div class="block">
             <x-label for="email" value="{{ __('Email') }}" />
-            <x-input id="email" class="block mt-1 w-full" type="email" name="email"
+            <input id="email" class="block w-full px-4 py-2 mt-1 rounded-full bg-white shadow-[0_0_20px_rgba(0,0,0,0.05)] border border-transparent focus:outline-none focus:ring-2 focus:ring-brown-600 focus:border-transparent font-poppins" type="email" name="email"
                 :value="old('email', $request->email)" required autofocus />
         </div>
 
         <div class="mt-4">
             <x-label for="password" value="{{ __('Password') }}" />
-            <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
+            <input id="password" class="block w-full px-4 py-2 mt-1 rounded-full bg-white shadow-[0_0_20px_rgba(0,0,0,0.05)] border border-transparent focus:outline-none focus:ring-2 focus:ring-brown-600 focus:border-transparent font-poppins" type="password" name="password" required
                 autocomplete="new-password" />
         </div>
 
         <div class="mt-4">
             <x-label for="password_confirmation" value="{{ __('Confirm Password') }}" />
-            <x-input id="password_confirmation" class="block mt-1 w-full" type="password"
+            <input id="password_confirmation" class="block w-full px-4 py-2 mt-1 rounded-full bg-white shadow-[0_0_20px_rgba(0,0,0,0.05)] border border-transparent focus:outline-none focus:ring-2 focus:ring-brown-600 focus:border-transparent font-poppin" type="password"
                 name="password_confirmation" required autocomplete="new-password" />
         </div>
 
-        <x-button class="mt-4 w-full text-lg transition rounded-lg bg-merah-2 opacity-90 hover:opacity-100 font-poppins">
+        <div class="flex flex-col items-center mt-3">
+            <x-button class="mt-4 items-center justify-center px-8 py-3 text-lg font-bold text-white transition-all rounded-full shadow-lg bg-gradient-to-b from-pink-500 to-indigo-900 hover:brightness-110 hover:scale-105 font-poppins">
             Reset Password
-        </x-button>
+            </x-button>
+        </div>
     </form>
 </x-guest-layout>
